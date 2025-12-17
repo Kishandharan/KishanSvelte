93eaca1ba29452b3362350a6fd4ce7184bc822ea
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-12-17 05:21:05 +0400

    Added some stuff

---
+++ ts_svelte7/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let joke : string = "Press the button first to get a joke"; 
+    async function handle1(){
+        let dat1 = await fetch("https://v2.jokeapi.dev/joke/Programming?type=single");    
+        let dat2 = await dat1.json();
+        let dat3 = dat2.joke;
+        joke=dat3;
+    }
+</script>
+
+<p class="joke">{joke}</p>
+<button onclick={handle1}>Generate Joke</button>
+
+<style>
+</style>
