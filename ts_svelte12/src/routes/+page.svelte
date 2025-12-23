b847f2abcde6ff75cc4e0616cbed8b9ad2e1478f
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-12-23 15:03:36 +0400

    Added some stuff, oh yeah!!

---
+++ ts_svelte12/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import "../app.css";
+    let output : string;
+    async function handle1(){
+        let data1 = await fetch("https://v2.jokeapi.dev/joke/Programming?type=single");
+        let data2 = await data1.json();
+        let data3 = data2.joke;
+        output = data3;
+    }
+</script>
+
+<p>{output}</p>
+<button onclick={handle1}>Get joke</button>
+
+<style></style>
---
+++ ts_svelte13/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let visibility:boolean = false;
+    function handle1(){
+        visibility = !(visibility);
+    }
+</script>
+
+<button on:click={handle1}>Toggle Visibility</button>
+{#if visibility}
+    <p>Click the button, and I will be visible.</p>
+{/if}
