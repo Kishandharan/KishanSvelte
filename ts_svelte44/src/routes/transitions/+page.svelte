f9204bf8f3317e2f2bf6d00a4bbb8f581beeb3c8
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-01-22 13:56:39 +0530

    Added some stuff

---
+++ ts_svelte44/src/routes/transitions/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import {fade} from "svelte/transition";
+  let showValue = $state(false);
+</script>
+
+<button onclick={ ()=> {showValue = !showValue} }>Toggle Visibility</button>
+{#if showValue}
+  <p transition:fade>I am visible</p>
+{/if}
