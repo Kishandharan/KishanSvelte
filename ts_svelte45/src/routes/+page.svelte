43b54949ad8d959119d84ae51e06f0c2740426a4
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-04-10 12:31:03 +0530

    Just got a little bored. So, coded in SvelteKit

---
+++ ts_svelte45/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let list1 = $state([]);
+  let inputElm;
+
+  function handleClick(){
+    list1.push(inputElm.value);
+  }
+</script>
+
+<input bind:this={inputElm}>
+<button onclick={handleClick}>Add item</button>
+{#each list1 as item}
+  <p>{item}</p>
+{/each}
